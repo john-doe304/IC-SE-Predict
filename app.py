bb4a7d0c5a328f09a6e0502cf5cfea7c02e1f579
john-doe304 <1469205174@qq.com>
2025-12-16 09:43:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -581,7 +581,7 @@ if submit_button:
 
                 # 显示预测结果
                 st.write("Prediction Results (Essential Models):")
-                st.markdown(
+                #st.markdown(
                     "**Note:** WeightedEnsemble_L2 is a meta-model combining predictions from other models.")
                 results_df = pd.DataFrame(predictions_dict)
                 st.dataframe(results_df.iloc[:1,:])
@@ -592,6 +592,7 @@ if submit_button:
 
             except Exception as e:
                 st.error(f"Model loading failed: {str(e)}")
+
 
 
 
